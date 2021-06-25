06d920a3f8ed38ad913c9d465a368f918c3108df
Mahadi92 <mahadi.hasan.937524.mh@gmail.com>
2021-06-25 20:06:00 +0600

    Mongo Connected

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors')
+const MongoClient = require('mongodb').MongoClient;
+const ObjectId = require('mongodb').ObjectId;
+require('dotenv').config();
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.sioj4.mongodb.net/${process.env.DB_HOST}?retryWrites=true&w=majority`;
+
+const app = express()
+app.use(bodyParser.json());
+app.use(cors());
+
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+    const postCollection = client.db("blogino").collection("posts");
+
+    console.log(err);
+
+    // Add Post 
+    app.post('/addPost', (req, res) => {
+        const post = req.body;
+        postCollection.insertOne(post)
+            .then(results => {
+                res.send(results.insertedCount > 0)
+            })
+    })
+
+    // Read Posts
+    app.get('/posts', (req, res) => {
+        postCollection.find({})
+            .toArray((err, posts) => {
+                res.send(posts)
+            })
+    })
+
+
+    app.get('/', function (req, res) {
+        res.send('hello world')
+    });
+
+
+
+})
+app.listen(process.env.PORT || 5000);
+
